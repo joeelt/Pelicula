eb0b0c449ca7f939f6a0d0ed7599bfe485c483a2
Joel Treboles <j.treboles.pilataxi@gmail.com>
2023-02-25 17:24:32 +0100

    Commit 2: Boton Volver.

--- app/src/main/java/com/example/pelicula/Fragment2.java
+++ app/src/main/java/com/example/pelicula/Fragment2.java
@@ -1,5 +1,6 @@
 package com.example.pelicula;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 
 public class Fragment2 extends Fragment {
 
+    private Button mBTN_Regresar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+//        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+       mBTN_Regresar = view.findViewById(R.id.BTN_Volver);
+       mBTN_Regresar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getContext(), MainActivity.class);
+               intent.putExtra("mensaje", "cartelera de peliculas");
+
+               startActivity(intent);
+           }
+       });
+        return view;
     }
 }
